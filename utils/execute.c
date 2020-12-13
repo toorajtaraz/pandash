f9e49d99d61de480ee1cc753bcec5a2cc3a2142b
tooraj taraz <tooraj.info@gmail.com>
2020-12-13 16:55:33 +0330

    implementing command executer

--- utils/execute.c
+++ utils/execute.c
@@ -1,6 +1,43 @@
+#include <stdio.h>
 #include <unistd.h>
 #include "../headers/execute.h"
 #include<sys/wait.h> 
+#include <string.h>
+#include "../headers/commands.h"
+#include <stdlib.h>
+
+void handle_built_in(Parsed* parsed_command) {
+   if (strstr(parsed_command->command, "help")) {
+       help();
+       return;
+   }
+   if (strstr(parsed_command->command, "cd")) {
+       if (!change_directory(parsed_command->flag_count == 0 ? NULL : parsed_command->flags[1])) {
+            fprintf(stderr, "COULD NOT CHANGE DIRECTORY\n");
+       }
+       return;
+   }
+   if (strstr(parsed_command->command, "exit")) {
+       exit_pandash(); 
+       return;
+   } 
+   if (strstr(parsed_command->command, "pandash_talk")) {
+        if (parsed_command->flag_count <= 2) {
+            fprintf(stderr, "YOU NEED TO PROVIDE 2 VALUES, YOUR ID AND THE OTHER SHELL\n");
+            return;
+        }
+        pid_t pid = fork();
+        if (pid == -1) {
+            return;
+        } else if (pid == 0) {
+            pandash_talk(parsed_command->flags[1], parsed_command->flags[2]);
+            abort();
+        } else {
+            wait(NULL);
+        }
+       return;
+   }  
+}
 
 int spawn_proc(Parsed *parsed_command) {
     pid_t pid = fork();
@@ -8,11 +45,15 @@ int spawn_proc(Parsed *parsed_command) {
         return -1;
     } else if (pid == 0) {
         execvp(parsed_command->flags[0], parsed_command->flags);
+        perror("could not execute");
+        abort();
+    } else {
+        wait(NULL);
     }
     return pid;
 }
 
-int spawn_proc_fork(int in, int out, Parsed* parsed_command) {
+int spawn_proc_fork(int in, int out, Parsed* parsed_command) {/*here the parent should not wait otherwise the program will hang*/
     pid_t pid = fork();
     if (pid == -1) {
         return -1;
@@ -25,18 +66,19 @@ int spawn_proc_fork(int in, int out, Parsed* parsed_command) {
             dup2 (out, 1);
             close (out);
         }
-        return execvp (parsed_command->flags[0], parsed_command->flags);
-    } else
-        wait(NULL);
+        execvp (parsed_command->flags[0], parsed_command->flags);
+        perror("could not execute");
+        abort();
+    }
     return pid;
 }
 
 int fork_pipes(Parsed* parsed_command) {
-    int i;
+    int i = 0;
     pid_t pid;
     int in, fd [2];
     in = 0;
-    for (;parsed_command->command[0] != '\0';) {
+    for (;parsed_command->command[0] != '\0'; i++) {
         if ((parsed_command->next_command)->command[0] == '\0') {
             break;
         }
@@ -48,7 +90,9 @@ int fork_pipes(Parsed* parsed_command) {
     }
     if (in != 0)
         dup2 (in, 0);
-    return execvp (parsed_command->flags[0], parsed_command->flags);
+    execvp (parsed_command->flags[0], parsed_command->flags);
+    perror("could not execute");
+    abort();
 }
 
 int pipes(Parsed* parsed_command) {
@@ -56,7 +100,7 @@ int pipes(Parsed* parsed_command) {
     if (pid == -1) {
         return -1;
     } else if (pid == 0) {
-        fork_pipes(parsed_command);
+        return fork_pipes(parsed_command);
     } else {
         wait(NULL);
     }
