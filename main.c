faa688d93db9b229b15c51962e5d8d15d181ee77
tooraj taraz <tooraj.info@gmail.com>
2021-06-23 13:50:43 +0430

    formatting main

--- main.c
+++ main.c
@@ -1,29 +1,28 @@
+#include "headers/exe_file.h"
+#include "headers/shell.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include<sys/wait.h>
-#include "headers/shell.h"
-#include "headers/exe_file.h"
 
-int main(int argc, char** args) {
-    switch (argc) {
-        case 1 : {
-             pandash(); 
-             break; 
-        }
-        case 2 : {
-             if (fork() == 0) {
-                process_file(args[1]);
-                abort(); 
-             } else {
-                wait(NULL);
-                break;        
-             }
-        }
-        default : {
-            fprintf(stderr, "TOO MANY ARGS\n");
-            exit(EXIT_FAILURE);      
-        }
+int main(int argc, char **args) {
+  switch (argc) {
+  case 1: {
+    pandash();
+    break;
+  }
+  case 2: {
+    if (fork() == 0) {
+      process_file(args[1]);
+      abort();
+    } else {
+      wait(NULL);
+      break;
     }
+  }
+  default: {
+    fprintf(stderr, "TOO MANY ARGS\n");
+    exit(EXIT_FAILURE);
+  }
+  }
 }
